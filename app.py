929898caf02324daa25d795baf68e695863d8819
Saul Aznarez <saul.aznarez-rodriguez@capgemini.com>
2025-05-05 11:57:38 +0200

    Organizacion del codigo

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 import streamlit as st
 import time
-import io
 import os
 from datetime import datetime
-from utils.pdf_extractor import extract_text_from_pdf
-from fpdf import FPDF
+from utils.pdf_extractor import (extract_text_from_pdf, generate_pdf, clean_text)
 from database.db_manager import (registrar_usuario, login, obtener_documentos_por_rfp_y_usuario,
     guardar_rfp, eliminar_documento_usuario, guardar_documento_usuario, obtener_documento_usuario, obtener_todas_rfps_por_usuario,
     actualizar_documento_usuario, es_correo_valido, obtener_todos_documentos_por_usuario)
@@ -57,41 +55,6 @@ def logout():
     st.session_state["current_category"] = "Home"
     st.session_state["current_page"] = "Mis RFPs"
     st.session_state["show_welcome_message"] = True
-
-def clean_text(text):
-    return ''.join(c if ord(c) < 256 else '?' for c in text)
-
-def generate_pdf(content):
-    pdf = FPDF()
-    pdf.add_page()
-
-    logo_path = "BID_AI_Logotype_Light.jpeg"
-
-    if os.path.exists(logo_path):
-        try:
-            logo_width = 50
-            page_width = pdf.w
-            x_position = (page_width - logo_width) / 2
-            pdf.image(logo_path, x=x_position, y=10, w=logo_width)
-            pdf.ln(50)
-        except RuntimeError as e:
-            st.error(f"⚠️ Error al insertar el logo en el PDF: {e}")
-    else:
-        st.warning(f"⚠️ Logo no encontrado en: {logo_path}")
-
-    pdf.set_font("Arial", 'B', size=16)
-    pdf.cell(0, 10, "Análisis Automático de RFP", ln=True, align="C")
-    pdf.ln(10)
-
-    pdf.set_font("Arial", size=12)
-    content = clean_text(content)
-    pdf.multi_cell(0, 10, content)
-
-    pdf_buffer = io.BytesIO()
-    pdf_output = pdf.output(dest='S').encode('latin-1', 'replace')
-    pdf_buffer.write(pdf_output)
-    pdf_buffer.seek(0)
-    return pdf_buffer
 
 # Función para restablecer análisis al cambiar de categoría o subcategoría
 def reset_analysis():
--- utils/pdf_extractor.py
+++ utils/pdf_extractor.py
@@ -1,4 +1,8 @@
 from PyPDF2 import PdfReader
+from fpdf import FPDF
+import os
+import streamlit as st
+import io
 
 def extract_text_from_pdf(file_path):
     reader = PdfReader(file_path)
@@ -6,3 +10,38 @@ def extract_text_from_pdf(file_path):
     for page in reader.pages:
         text += page.extract_text()
     return text
+
+def generate_pdf(content):
+    pdf = FPDF()
+    pdf.add_page()
+
+    logo_path = "BID_AI_Logotype_Light.jpeg"
+
+    if os.path.exists(logo_path):
+        try:
+            logo_width = 50
+            page_width = pdf.w
+            x_position = (page_width - logo_width) / 2
+            pdf.image(logo_path, x=x_position, y=10, w=logo_width)
+            pdf.ln(50)
+        except RuntimeError as e:
+            st.error(f"⚠️ Error al insertar el logo en el PDF: {e}")
+    else:
+        st.warning(f"⚠️ Logo no encontrado en: {logo_path}")
+
+    pdf.set_font("Arial", 'B', size=16)
+    pdf.cell(0, 10, "Análisis Automático de RFP", ln=True, align="C")
+    pdf.ln(10)
+
+    pdf.set_font("Arial", size=12)
+    content = clean_text(content)
+    pdf.multi_cell(0, 10, content)
+
+    pdf_buffer = io.BytesIO()
+    pdf_output = pdf.output(dest='S').encode('latin-1', 'replace')
+    pdf_buffer.write(pdf_output)
+    pdf_buffer.seek(0)
+    return pdf_buffer
+
+def clean_text(text):
+    return ''.join(c if ord(c) < 256 else '?' for c in text)
